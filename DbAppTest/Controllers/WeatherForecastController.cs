17c5f0f32f194cf51d0dcf56e1803815c89afb33
Catalin Sbora <catalin.sbora@gmail.com>
2025-05-13 16:10:54 +0300

    use managed identity

--- DbAppTest/Controllers/WeatherForecastController.cs
+++ DbAppTest/Controllers/WeatherForecastController.cs
@@ -1,6 +1,12 @@
+using Azure.Core;
 using DbAppTest.datasource;
 using DbAppTest.model;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using Azure.Identity;
+using Azure.Core;
+
+
 
 namespace DbAppTest.Controllers
 {
@@ -23,8 +29,45 @@ namespace DbAppTest.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherEntry> Get()
+        public async Task<IEnumerable<WeatherEntry>> Get()
         {
+           
+            // Uncomment the following lines corresponding to the authentication type you want to use.
+            // For system-assigned identity.
+             var sqlServerTokenProvider = new DefaultAzureCredential();
+
+            // For user-assigned identity.
+            // var sqlServerTokenProvider = new DefaultAzureCredential(
+            //     new DefaultAzureCredentialOptions
+            //     {
+            //         ManagedIdentityClientId = Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_CLIENTID");
+            //     }
+            // );
+
+            // For service principal.
+            // var tenantId = Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_TENANTID");
+            // var clientId = Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_CLIENTID");
+            // var clientSecret = Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_CLIENTSECRET");
+            // var sqlServerTokenProvider = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            // Acquire the access token. 
+            AccessToken accessToken = await sqlServerTokenProvider.GetTokenAsync(
+                new TokenRequestContext(scopes: new string[]
+                {
+                        "https://ossrdbms-aad.database.windows.net/.default"
+                }));
+
+            // Combine the token with the connection string from the environment variables provided by Service Connector.
+            string connectionString =
+                $"{Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_CONNECTIONSTRING")};Password={accessToken.Token}";
+
+            // Establish the connection.
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                Console.WriteLine("Opening connection using access token...");
+                connection.Open();
+                Console.WriteLine("DATABASE CONNECTION OPENED");
+            }
             return _context.WeatherEntries.ToList(); 
            
         }
